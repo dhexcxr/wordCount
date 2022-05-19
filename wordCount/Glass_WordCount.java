b743ace5efbcb01eb8f8635891f16af3f2923ca4
M. Corey Glass <dhexcxr@gmail.com>
2022-05-19 19:22:09 -0400

    feat: initial commit

---
+++ wordCount/Glass_WordCount.java
@@ -0,0 +1,45 @@
+//Author Name: M. Corey Glass
+//Date: 5.19.22
+//Program Name: WroneController
+//Class Name: Glass_WordCount
+//Purpose: count words in supplied file, display the words sorted from most frequent to least
+
+package wordCount;
+
+public class Glass_WordCount {
+
+	public static void main(String[] args) {
+
+		getFile(args);
+		countWords();
+		displayResults();
+		
+	}
+
+	private static void getFile(String[] args) {
+		System.out.println("We're getting the file");
+		
+		// read file from command line arg
+
+		// if no file on command line, ask user for file name
+
+		// ask again if file not found
+	}
+	
+	private static void countWords() {
+		System.out.println("We're reading the file");
+		
+		// count word occurrences
+		
+	}
+	
+	private static void displayResults() {
+		System.out.println("We're sorting the results");
+		
+		// sort results
+		
+		// display results
+		
+	}
+
+}
