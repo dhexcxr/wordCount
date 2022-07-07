311de53199442b156f2298d435b3b99d54fd2199
M. Corey Glass <dhexcxr@gmail.com>
2022-07-07 18:46:10 -0400

    feat: add JUnit testing

---
+++ wordCount/WordCountTest.java
@@ -0,0 +1,24 @@
+package wordCount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordCountTest {
+
+	@Test
+	void openFileTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void countWordsTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void displayResultsTest() {
+		fail("Not yet implemented");
+	}
+
+}
